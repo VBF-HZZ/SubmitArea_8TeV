6e18d0907f7c7e4bb1ed3dcef1bd1bfd77836f3b
Hengne Li <Hengne.Li@cern.ch>
2014-07-11 07:51:28 -0400

    modify script names

--- createAllDataCfgs.sh
+++ createAllDataCfgs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-./Create_DATA_2012AB_cfg.sh Data_2012A
-./Create_DATA_2012AB_cfg.sh Data_2012B
-./Create_DATA_2012AB_cfg.sh Data_2012C
-./Create_DATA_2012AB_cfg.sh Data_2012D
+#./Create_DATA_2012ABCD_cfg.sh Data_2012A
+./Create_DATA_2012ABCD_cfg.sh Data_2012B
+./Create_DATA_2012ABCD_cfg.sh Data_2012B_try2
+./Create_DATA_2012ABCD_cfg.sh Data_2012C
+./Create_DATA_2012ABCD_cfg.sh Data_2012D
 
