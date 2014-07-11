d5ef70fa37494d7f25ea1fff7ecf6017779bca58
Hengne Li <Hengne.Li@cern.ch>
2014-07-11 08:11:19 -0400

    add scripts to list all path tuples for data.

---
+++ listAllDataPatTuples.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#./listPatTuples.sh data/53X/Legacy_06.08/2012A
+#mv patTuples Data_2012A
+./listPatTuples.sh data/53X/Legacy_06.08/2012B
+mv patTuples Data_2012B
+./listPatTuples.sh data/53X/Legacy_06.08/2012B_try2
+mv patTuples Data_2012B_try2
+./listPatTuples.sh data/53X/Legacy_06.08/2012C
+mv patTuples Data_2012C
+./listPatTuples.sh data/53X/Legacy_06.08/2012D
+mv patTuples Data_2012D
